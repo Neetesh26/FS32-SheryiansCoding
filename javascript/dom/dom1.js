549b3d8ff037861f8ece6f0ef36c4bb8e5ab6946
Neetesh Prajapati <139002314+Neetesh26@users.noreply.github.com>
2025-08-02 22:17:32 +0530

    2 august

--- javascript/dom/dom1.js
+++ javascript/dom/dom1.js
@@ -1,9 +1,9 @@
-// let ul = document.querySelector("ul")
+let ul = document.querySelector("ul")
 
 
-// ul.addEventListener("click",function(dets){
-//     let r = Math.floor(Math.random()*256)
-//     let g = Math.floor(Math.random()*256)
-//     let b = Math.floor(Math.random()*256)
-//     dets.target.style.color = ` rgba(${r}, ${g}, ${b})`;
-// })
+ul.addEventListener("click",function(dets){
+    let r = Math.floor(Math.random()*256)
+    let g = Math.floor(Math.random()*256)
+    let b = Math.floor(Math.random()*256)
+    dets.target.style.color = ` rgba(${r}, ${g}, ${b})`;
+})
